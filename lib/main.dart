58abc6878f237aa4c4affe5be1e7df85b55748cc
Seiichiro Tanaka <seiichi3141@gmail.com>
2025-02-05 22:46:06 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:chinsukou_clicker/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ちんすこうクリッカー',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
